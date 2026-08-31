e8a39550d5c87513365d0ee7279c3447d0c78dd2
mayonesevideo <mayonesevideo@gmail.com>
2026-08-31 13:32:43 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -68,8 +68,9 @@ const storage = multer.diskStorage({
     cb(null, tempDir);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e4);
+    const cleanOriginalName = file.originalname.replace(/[^a-zA-Z0-9.-]/g, '_');
+    cb(null, uniqueSuffix + '-' + cleanOriginalName);
   }
 });
 
